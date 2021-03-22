c909bcbad16b0e8778d5981d8b7f14af8f067cc6
git-sravya <s.sravya@yahoo.com>
2021-03-22 15:16:10 +0530

    Error code

--- jenkinsnew/Program.cs
+++ jenkinsnew/Program.cs
@@ -10,7 +10,7 @@ namespace jenkinsnew
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello")
         }
     }
 }
